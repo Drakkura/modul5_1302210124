f57c0055c85c6b1c17ed68b5a0468eb730e2e36d
PC12-IFLAB <PC12-IFLAB>
2023-03-24 09:16:14 +0700

    “menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul5_1302210124;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302210124
+{
+    internal class SimpleDataBase_1302210124<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public void SimpleDataBase() { 
+            this.storedData= new List<T>();
+            this.inputDates= new List<DateTime>();
+        }
+
+        public void addNewData(T data) { 
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void printAllData() {
+            for (int i = 0; i < storedData.Count; i++) {
+                Console.WriteLine("Data " + i+1 + " berisi: "+ Convert.ToString(storedData[i]+", yang disimpan pada waktu: " + inputDates[i]));
+            }
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+            SimpleDataBase_1302210124<int> data = new SimpleDataBase_1302210124<int>();
+
+            data.addNewData(13);
+            data.addNewData(02); 
+            data.addNewData(21);
+
+            data.printAllData();
+        }
+    }
+}
